566fb96caf2de27a4bb286e97128ceb2a056d970
Julio Moreira B. Barros <juliogx@hotmail.com>
2018-04-25 00:36:59 -0300

    main class (selecao e telinha final)

--- src/br/unicamp/tretacamp/Jogo.java
+++ src/br/unicamp/tretacamp/Jogo.java
@@ -1,14 +1,16 @@
-package br.unicamp.tretacamp;/**
- * @author Júlio Moreira Blás de Barros (julio.barros@movile.com)
- * @since 17/04/18
- */
+package br.unicamp.tretacamp;
 
+import br.unicamp.tretacamp.modelo.Drego;
+import br.unicamp.tretacamp.modelo.Tipo;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.Scanner;
+
+import static br.unicamp.tretacamp.Jogo.Perdedor.JOGADOR;
 
 public class Jogo extends Application {
 
@@ -18,24 +20,119 @@ public class Jogo extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    StackPane raiz = new StackPane();
-    Label lblMensagem = new Label();
-    Button botao = new Button();
+    try {
+      final Scanner sc = new Scanner(System.in);
 
-    lblMensagem.setTranslateX(0.0);
-    lblMensagem.setText("Estou aprendendo JavaFX!");
-    botao.setDefaultButton(true);
-    botao.setText("booora");
-    botao.setOnAction(event -> {
-      System.out.println("clicou");
-    });
-    botao.setTranslateY(30.0);
-    raiz.getChildren().add(lblMensagem);
-    raiz.getChildren().add(botao);
+      final Drego mago = new Drego(
+        "mago", "mago.png", 10.0, 10.0, Tipo.AGUA, null);
+      final Drego espotenique = new Drego(
+        "espotenique", "espotenique.png", 10.0, 10.0, Tipo.AGUA, null);
+      final Drego monho = new Drego(
+        "monho", "monho.gif", 10.0, 10.0, Tipo.AGUA, null);
+      final Drego cobroso = new Drego(
+        "cobroso", "cobroso.png", 10.0, 10.0, Tipo.AGUA, null);
 
-    Scene cena = new Scene(raiz, 250, 100);
-    primaryStage.setTitle("Aprendendo JavaFX");
-    primaryStage.setScene(cena);
-    primaryStage.show();
+      final Drego[] dregos = { mago, espotenique, monho, cobroso };
+
+      final Drego jogador = dregos[selecionaDrego("jogador", sc)];
+      limparConsole();
+      System.out.println("Drego escolhido para o jogador: " + jogador.getNome());
+      Thread.sleep(200);
+      limparConsole();
+
+      final Drego inimigo = dregos[selecionaDrego("inimigo", sc)];
+      limparConsole();
+      System.out.println("Drego escolhido para o inimigo: " + inimigo.getNome());
+      Thread.sleep(200);
+      limparConsole();
+
+      System.out.println("Inicio da batalha");
+
+      do {
+        System.out.println("Seu turno: ");
+        // menu do turno do jogador, mostra habilidades e deixa ele selecionar
+
+        System.out.println("Turno do oponente: ");
+        // aleatorio?
+        break;
+
+
+      } while (jogador.getVida() > 0 && inimigo.getVida() > 0);
+
+      System.out.println("Fim");
+
+      //definir aqui se o jogador perdeu
+      final String perdedor = JOGADOR;
+
+
+      // Aqui ficaria a parte visual
+      // para a primeira entrega, essa parte está
+      // limitada e a batalha acontece na linha de comando.
+      // por enquanto só mostramos as imagens
+      {
+        ImageView imagemJog = new ImageView(
+          "resources/" + jogador.getVisual());
+        ImageView imagemIni = new ImageView(
+          "resources/" + inimigo.getVisual());
+
+        StackPane raiz = new StackPane();
+        Scene cena = new Scene(raiz, 1000, 400);
+        imagemJog.setTranslateX(-400);
+        imagemIni.setTranslateX(400);
+        if (perdedor == JOGADOR) {
+          imagemJog.setRotate(90.0);
+        } else {
+          imagemIni.setRotate(90.0);
+        }
+        raiz.getChildren().add(imagemJog);
+        raiz.getChildren().add(imagemIni);
+        primaryStage.setTitle("Treta Camp - imagem da batalha");
+        primaryStage.setScene(cena);
+        primaryStage.show();
+      }
+      // fim da parte visual
+
+    } catch (Exception e) {
+      System.out.println("Problema inesperado: " + e.getMessage());
+    }
+  }
+
+  private int selecionaDrego(String entidade, Scanner sc) {
+    System.out.println("Selecione o drego que deseja para o " + entidade);
+    while (true) {
+      System.out.println("1. Mago");
+      System.out.println();
+      System.out.println("2. Espotenique");
+      System.out.println();
+      System.out.println("3. Monho");
+      System.out.println();
+      System.out.println("4. Cobroso");
+      System.out.println();
+
+      try {
+        final int opcao = sc.nextInt();
+        if (opcao < 0 || opcao > 4) {
+          limparConsole();
+          System.out.println("Opcao invalida. Tente novamente...");
+          continue;
+        }
+
+        return opcao - 1; // opcao vai ser a posicao no array
+
+      } catch (Exception e) {
+        limparConsole();
+        System.out.println("Problema na leitura. Tente novamente...");
+      }
+    }
+  }
+
+  private void limparConsole() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
+
+  static class Perdedor {
+    static final String JOGADOR = "jog";
+    static final String INIMIGO = "ini";
   }
 }
