f66c1cb389e256b81beef7e21b9ffaf9c1a957e4
flavio.reginato <flavio.reginato@playkids.com>
2018-04-25 01:14:52 -0300

    Implementação inicial PoderEspecial

--- src/br/unicamp/tretacamp/modelo/PoderEspecial.java
+++ src/br/unicamp/tretacamp/modelo/PoderEspecial.java
@@ -1,7 +1,13 @@
 package br.unicamp.tretacamp.modelo;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PoderEspecial extends Poder {
 	private Condicao condicao;
+	private final static Map<Tipo, List<Tipo>> tipoFraquezas = popularTipoFraquesas();
 	
 	public PoderEspecial(String nome, double dano, double custo, Condicao condicao) {
 		super(nome, dano, custo);
@@ -13,7 +19,9 @@ public class PoderEspecial extends Poder {
 		if (condicao.verificar(conjurante)) {
 			super.aplicar(conjurante, atingido);
 			
-			// extras
+			double porcentagemAumento = getPorcentagemAumentoEfeito(conjurante, atingido);
+			
+			atingido.diminuirVida(porcentagemAumento * getDanoInstantaneo());
 		}
 	}
 
@@ -25,4 +33,26 @@ public class PoderEspecial extends Poder {
 		this.condicao = condicao;
 	}
 	
+	public double getPorcentagemAumentoEfeito(Drego conjurante, Drego atingido) {
+		List<Tipo> fraquezasTipoAtingido = tipoFraquezas.get(atingido.getTipo());
+		
+		if (fraquezasTipoAtingido.contains(conjurante.getTipo())) {
+			return 0.25;
+		}
+		
+		return 0.1;
+	}
+	
+	private static Map<Tipo, List<Tipo>> popularTipoFraquesas() {
+		Map<Tipo, List<Tipo>> tipoFraquezas = new HashMap<Tipo, List<Tipo>>();
+		
+		tipoFraquezas.put(Tipo.AGUA, Arrays.asList(Tipo.TERRA, Tipo.METAL));
+		tipoFraquezas.put(Tipo.FOGO, Arrays.asList(Tipo.AGUA, Tipo.VENTO));
+		tipoFraquezas.put(Tipo.METAL, Arrays.asList(Tipo.FOGO, Tipo.SONORO));
+		tipoFraquezas.put(Tipo.SONORO, Arrays.asList(Tipo.TERRA, Tipo.AGUA));
+		tipoFraquezas.put(Tipo.TERRA, Arrays.asList(Tipo.FOGO, Tipo.VENTO));
+		tipoFraquezas.put(Tipo.VENTO, Arrays.asList(Tipo.SONORO, Tipo.METAL));
+		
+		return tipoFraquezas;
+	}
 }
